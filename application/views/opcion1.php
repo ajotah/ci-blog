b0427097055d11998358c0ca76e4089210b3c9d8
abel-k <info@abeljstudio.com>
2018-11-22 18:50:07 +0100

    Inicio
    Inicio desarrollo. Se implementa CodeIgniter 3.

---
+++ application/views/opcion1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Opción 1</title>
+  <meta charset="utf-8">
+
+</head>
+<body>
+
+<p>Esto es la opción 1 picha</p>
+
+</body>
+</html>
---
+++ application/views/principal.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PRINCIPAL</title>
+  <meta charset="utf-8">
+
+</head>
+<body>
+
+<p>Principal, funciona?</p>
+
+</body>
+</html>
